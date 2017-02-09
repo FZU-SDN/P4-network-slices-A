66018725237d1b4a9f21151f8c1dc9ee25ed334c
Wasdns <952693358@qq.com>
2017-02-09 21:40:28 +0800

    change commands.txt
    change commands.txt

--- slices_demo4/add_entry.sh
+++ slices_demo4/add_entry.sh
@@ -1,6 +1,14 @@
 # FuZhou University, SDN Lab.
 # changed by Chen, 2017/2/7
 
+# Commands:
+#
+# sudo ./runtime_CLI.py --pre SimplePreLAG --thrift-port [thrift_port] < commands.txt
+#
+# or
+#
+# sudo ./simple_switch_CLI --thrift-port [thrift_port] < commands.txt
+
 sudo ./simple_switch_CLI --thrift-port 22222 < commands.txt
 sudo ./simple_switch_CLI --thrift-port 22223 < commands.txt
 sudo ./simple_switch_CLI --thrift-port 22224 < commands.txt
